e13048dd5dc0fc1f69eb0e5c9f42efbfcb3bc664
Perry Mitchell <perry@perrymitchell.net>
2016-10-24 22:43:30 +0300

    Remove incompatible code for 0.12

--- source/adapter/response.js
+++ source/adapter/response.js
@@ -3,7 +3,7 @@ module.exports = {
     handleResponseCode: function(response) {
         var status = parseInt(response.status, 10);
         if (status >= 400) {
-            throw new Error(`Invalid response: ${status} ${response.statusText}`);
+            throw new Error("Invalid response: " + status + " " + response.statusText);
         }
         return response;
     }
