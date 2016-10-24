d46901bb19a1c1358526f20ba8d12b3ea8cc571a
Perry Mitchell <perry@perrymitchell.net>
2016-10-24 22:40:59 +0300

    Update URL tools to escape username and password - fixes #5

--- source/url.js
+++ source/url.js
@@ -1,9 +1,13 @@
+var queryString = require("querystring");
+
 module.exports = {
 
     implantCredentials: function(url, username, password) {
         return (username && username.length > 0) ?
-            url.replace(/(https?:\/\/)/i, "$1" + username + ":" + password + "@") :
-            url;
+            url.replace(
+                /(https?:\/\/)/i,
+                "$1" + queryString.escape(username) + ":" + queryString.escape(password) + "@"
+            ) : url;
     },
 
     /**
