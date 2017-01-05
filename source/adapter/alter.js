77e44794195f0f0c4487bb10cabb4990c7be3d45
balu <balu@mobile>
2017-01-05 20:52:14 +0100

    options for alter functions

--- source/adapter/alter.js
+++ source/adapter/alter.js
@@ -1,22 +1,41 @@
 var fetch = require("node-fetch");
+var deepmerge = require("deepmerge");
 
 var responseHandlers = require("./response.js");
 
+function getAlterItemDefaults() {
+    return {
+        headers: { },
+    };
+}
+
 module.exports = {
 
-    deleteItem: function deleteItem(url, targetPath) {
+    deleteItem: function deleteItem(url, targetPath, options) {
+        options = deepmerge.all([
+            getAlterItemDefaults(),
+            options || {}
+        ]);
         return fetch(url + targetPath, {
-                method: "DELETE"
+                method: "DELETE",
+                headers: options.headers
             })
             .then(responseHandlers.handleResponseCode);
     },
 
-    moveItem: function moveItem(url, filePath, targetFilePath) {
-        return fetch(url + filePath, {
-                method: "MOVE",
+    moveItem: function moveItem(url, filePath, targetFilePath, options) {
+        options = deepmerge.all([
+            getAlterItemDefaults(),
+            options || {},
+            {
                 headers: {
                     Destination: url + targetFilePath
                 }
+            }
+        ]);
+        return fetch(url + filePath, {
+                method: "MOVE",
+                headers: options.headers
             })
             .then(responseHandlers.handleResponseCode);
     }
