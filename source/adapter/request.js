508724b95bb5ffcabc85d222e8c20ca9d1688c10
Perry Mitchell <perry@perrymitchell.net>
2017-06-24 21:51:33 +0300

    Disable native window.fetch for browsers

--- source/adapter/request.js
+++ source/adapter/request.js
@@ -1,9 +1,11 @@
 var nodeFetch = require("node-fetch");
 
 var fetch = nodeFetch;
-if (typeof window === "object" && typeof window.fetch === "function") {
-    fetch = window.fetch;
-}
+
+// // For some reason window.fetch doesn't work in some cases
+// if (typeof window === "object" && typeof window.fetch === "function") {
+//     fetch = window.fetch;
+// }
 
 module.exports = function request(url, options) {
     return fetch(url, options);
