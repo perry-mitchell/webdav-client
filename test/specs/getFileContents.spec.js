cd5780764bf9f3b823f1cce58582d3f6e0d02115
Perry Mitchell <perry@perrymitchell.net>
2017-08-04 17:52:16 +0300

    Add tests for getFileContents

---
+++ test/specs/getFileContents.spec.js
@@ -0,0 +1,40 @@
+var path = require("path"),
+    fs = require("fs");
+
+var bufferEquals = require("buffer-equals");
+
+var SOURCE_BIN = path.resolve(__dirname, "../testContents/alrighty.jpg"),
+    SOURCE_TXT = path.resolve(__dirname, "../testContents/text document.txt");
+
+describe("getFileContents", function() {
+
+    beforeEach(function() {
+        this.client = createWebDAVClient(
+            "http://localhost:9988/webdav/server",
+            createWebDAVServer.test.username,
+            createWebDAVServer.test.password
+        );
+        clean();
+        this.server = createWebDAVServer();
+        return this.server.start();
+    });
+
+    afterEach(function() {
+        return this.server.stop();
+    });
+
+    it("reads a remote file into a buffer", function() {
+        return this.client.getFileContents("/alrighty.jpg").then(function(bufferRemote) {
+            var bufferLocal = fs.readFileSync(SOURCE_BIN);
+            expect(bufferEquals(bufferRemote, bufferLocal)).to.be.true;
+        });
+    });
+
+    it("reads a remote file into a string", function() {
+        return this.client.getFileContents("/text document.txt", { format: "text" }).then(function(stringRemote) {
+            var stringLocal = fs.readFileSync(SOURCE_TXT, "utf8");
+            expect(stringRemote).to.equal(stringLocal);
+        });
+    });
+
+});
