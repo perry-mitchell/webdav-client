08372a8efbbe4ac4cdc3bd840d34c0e01655e954
Perry Mitchell <perry@perrymitchell.net>
2017-08-06 12:58:46 +0300

    Remove commented code

--- test/specs/getQuota.spec.js
+++ test/specs/getQuota.spec.js
@@ -11,29 +11,6 @@ function useValidQuota() {
 
 describe("getQuota", function() {
 
-    // beforeEach(function() {
-    //     this.client = createWebDAVClient(
-    //         "http://localhost:9988/webdav/server",
-    //         createWebDAVServer.test.username,
-    //         createWebDAVServer.test.password
-    //     );
-    //     clean();
-    //     this.server = createWebDAVServer();
-    //     return this.server.start();
-    // });
-
-    // afterEach(function() {
-    //     return this.server.stop();
-    // });
-
-    // it("returns a quota object", function() {
-    //     return this.client.getQuota().then(function(quotaInfo) {
-    //         expect(contents).to.be.an.object;
-    //         expect(contents).to.have.property("available", "unlimited");
-    //         expect(contents).to.have.property("used", 0);
-    //     });
-    // });
-
     beforeEach(function() {
         // fake client, not actually used when mocking responses
         this.client = createWebDAVClient(
