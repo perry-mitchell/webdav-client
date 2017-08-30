5caf19142e227b0becc57147c85e442d88c5fe60
Perry Mitchell <perry@perrymitchell.net>
2017-08-30 18:31:37 +0300

    Use dist again

--- test/specs/index.js
+++ test/specs/index.js
@@ -4,7 +4,7 @@ require("./polyfill.js");
 
 const path = require("path");
 
-const createWebDAVClient = require("../../build/index.js");
+const createWebDAVClient = require("../../dist/index.js");
 const baseWebDAVServer = require("../server/index.js");
 
 const createWebDAVServer = baseWebDAVServer.webdavClient;
