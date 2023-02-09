e70bae61dc9ce605e5a191ccf4aec4634f42a6a2
Manuel Ottlik <28919003+manuelottlik@users.noreply.github.com>
2023-02-09 15:25:11 +0100

    removed blank line

--- source/tools/crypto.ts
+++ source/tools/crypto.ts
@@ -10,7 +10,6 @@ export function ha1Compute(
     ha1: string
 ): string {
     ha1 = ha1 || (md5(`${user}:${realm}:${pass}`) as string);
-
     if (algorithm && algorithm.toLowerCase() === "md5-sess") {
         return md5(`${ha1}:${nonce}:${cnonce}`) as string;
     }
