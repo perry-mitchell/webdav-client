92c1948af957b022a1d11480fd0b174c0f6c28dd
Perry Mitchell <perry@perrymitchell.net>
2023-10-31 20:10:47 +0200

    Configure cjs build in webpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,86 +7,158 @@ const DIST = path.resolve(__dirname, "./dist/web");
 
 const smp = new SpeedMeasurePlugin();
 
-module.exports = smp.wrap({
-    entry: path.resolve(__dirname, "./source/index.ts"),
+module.exports = [
+    {
+        entry: path.resolve(__dirname, "./source/index.ts"),
 
-    experiments: {
-        outputModule: true
-    },
+        experiments: {
+            outputModule: true
+        },
 
-    externals: [],
+        externalsType: "module",
 
-    externalsType: "module",
-
-    module: {
-        rules: [
-            {
-                test: /\.[jt]s$/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        ["@babel/preset-env", {
-                            targets: [
-                                "> 0.25%, not dead",
-                                "maintained node versions"
-                            ]
-                        }],
-                        "@babel/preset-typescript"
-                    ],
-                    plugins: [
-                        "babel-plugin-transform-async-to-promises"
-                    ]
-                },
-                resolve: {
-                    fullySpecified: false
+        module: {
+            rules: [
+                {
+                    test: /\.[jt]s$/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", {
+                                targets: [
+                                    "> 0.25%, not dead",
+                                    "maintained node versions"
+                                ]
+                            }],
+                            "@babel/preset-typescript"
+                        ],
+                        plugins: [
+                            "babel-plugin-transform-async-to-promises"
+                        ]
+                    },
+                    resolve: {
+                        fullySpecified: false
+                    }
                 }
+            ]
+        },
+
+        node: false,
+
+        output: {
+            filename: "index.js",
+            path: DIST,
+            environment: {
+                module: true
+            },
+            library: {
+                type: "module"
             }
-        ]
-    },
-
-    node: false,
-
-    output: {
-        filename: "index.js",
-        path: DIST,
-        environment: {
-            module: true
         },
-        library: {
-            type: "module"
-        }
-    },
 
-    plugins: [
-        new DefinePlugin({
-            WEB: "true"
-        })
-    ],
-
-    resolve: {
-        alias: {
-            "he": path.resolve(__dirname, "./util/he.stub.ts"),
-            "http": path.resolve(__dirname, "./util/http.stub.ts"),
-            "node-fetch": path.resolve(__dirname, "./util/node-fetch.stub.ts")
-        },
-        extensions: [".js"],
-        fallback: {
-            buffer: false,
-            crypto: false,
-            dns: false,
-            fs: false,
-            http: false,
-            https: false,
-            net: false,
-            path: false,
-            stream: false,
-            util: false
-        },
         plugins: [
-            // Handle .ts => .js resolution
-            new ResolveTypeScriptPlugin()
-        ]
-    },
+            new DefinePlugin({
+                WEB: "true"
+            })
+        ],
 
-    target: "web"
-});
+        resolve: {
+            alias: {
+                "he": path.resolve(__dirname, "./util/he.stub.ts"),
+                "http": path.resolve(__dirname, "./util/http.stub.ts"),
+                "node-fetch": path.resolve(__dirname, "./util/node-fetch.stub.ts")
+            },
+            extensions: [".js"],
+            fallback: {
+                buffer: false,
+                crypto: false,
+                dns: false,
+                fs: false,
+                http: false,
+                https: false,
+                net: false,
+                path: false,
+                stream: false,
+                util: false
+            },
+            plugins: [
+                // Handle .ts => .js resolution
+                new ResolveTypeScriptPlugin()
+            ]
+        },
+
+        target: "web"
+    },
+    {
+        entry: path.resolve(__dirname, "./source/index.ts"),
+
+        module: {
+            rules: [
+                {
+                    test: /\.[jt]s$/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", {
+                                targets: [
+                                    "> 0.25%, not dead",
+                                    "maintained node versions"
+                                ]
+                            }],
+                            "@babel/preset-typescript"
+                        ],
+                        plugins: [
+                            "babel-plugin-transform-async-to-promises"
+                        ]
+                    },
+                    resolve: {
+                        fullySpecified: false
+                    }
+                }
+            ]
+        },
+
+        node: false,
+
+        output: {
+            filename: "index.cjs",
+            path: DIST,
+            library: {
+                type: "commonjs"
+            }
+        },
+
+        plugins: [
+            new DefinePlugin({
+                WEB: "true"
+            })
+        ],
+
+        resolve: {
+            alias: {
+                "he": path.resolve(__dirname, "./util/he.stub.ts"),
+                "http": path.resolve(__dirname, "./util/http.stub.ts"),
+                "node-fetch": path.resolve(__dirname, "./util/node-fetch.stub.ts")
+            },
+            extensions: [".js"],
+            fallback: {
+                buffer: false,
+                crypto: false,
+                dns: false,
+                fs: false,
+                http: false,
+                https: false,
+                net: false,
+                path: false,
+                stream: false,
+                util: false
+            },
+            plugins: [
+                // Handle .ts => .js resolution
+                new ResolveTypeScriptPlugin()
+            ]
+        },
+
+        target: "web"
+    }
+].map(config => smp.wrap(config));
