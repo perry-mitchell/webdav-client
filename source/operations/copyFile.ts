a379d98a7479d1a133ede20074325058e5efbd3d
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-10 21:51:59 +0100

    enh: Allow to set `overwrite` for copy and move actions and allow shallow copy
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- source/operations/copyFile.ts
+++ source/operations/copyFile.ts
@@ -2,20 +2,33 @@ import { joinURL } from "../tools/url.js";
 import { encodePath } from "../tools/path.js";
 import { request, prepareRequestOptions } from "../request.js";
 import { handleResponseCode } from "../response.js";
-import { WebDAVClientContext, WebDAVMethodOptions } from "../types.js";
+import { CopyFileOptions, WebDAVClientContext, WebDAVMethodOptions } from "../types.js";
 
 export async function copyFile(
     context: WebDAVClientContext,
     filename: string,
     destination: string,
-    options: WebDAVMethodOptions = {}
+    options: CopyFileOptions = {}
 ): Promise<void> {
     const requestOptions = prepareRequestOptions(
         {
             url: joinURL(context.remoteURL, encodePath(filename)),
             method: "COPY",
             headers: {
-                Destination: joinURL(context.remoteURL, encodePath(destination))
+                Destination: joinURL(context.remoteURL, encodePath(destination)),
+                /**
+                 * From RFC4918 section 10.6: If the overwrite header is not included in a COPY or MOVE request,
+                 * then the resource MUST treat the request as if it has an overwrite header of value "T".
+                 *
+                 * Meaning the overwrite header is always set to "T" EXCEPT the option is explicitly set to false.
+                 */
+                Overwrite: options.overwrite === false ? "F" : "T",
+                /**
+                 * From RFC4918 section 9.8.3: A client may submit a Depth header on a COPY on a collection with a value of "0"
+                 * or "infinity". The COPY method on a collection without a Depth header MUST act as if
+                 * a Depth header with value "infinity" was included.
+                 */
+                Depth: options.shallow ? "0" : "infinity"
             }
         },
         context,
--- source/operations/moveFile.ts
+++ source/operations/moveFile.ts
@@ -2,20 +2,27 @@ import { joinURL } from "../tools/url.js";
 import { encodePath } from "../tools/path.js";
 import { request, prepareRequestOptions } from "../request.js";
 import { handleResponseCode } from "../response.js";
-import { WebDAVClientContext, WebDAVMethodOptions } from "../types.js";
+import { MoveFileOptions, WebDAVClientContext, WebDAVMethodOptions } from "../types.js";
 
 export async function moveFile(
     context: WebDAVClientContext,
     filename: string,
     destination: string,
-    options: WebDAVMethodOptions = {}
+    options: MoveFileOptions = {}
 ): Promise<void> {
     const requestOptions = prepareRequestOptions(
         {
             url: joinURL(context.remoteURL, encodePath(filename)),
             method: "MOVE",
             headers: {
-                Destination: joinURL(context.remoteURL, encodePath(destination))
+                Destination: joinURL(context.remoteURL, encodePath(destination)),
+                /**
+                 * From RFC4918 section 10.6: If the overwrite header is not included in a COPY or MOVE request,
+                 * then the resource MUST treat the request as if it has an overwrite header of value "T".
+                 *
+                 * Meaning the overwrite header is always set to "T" EXCEPT the option is explicitly set to false.
+                 */
+                Overwrite: options.overwrite === false ? "F" : "T"
             }
         },
         context,
