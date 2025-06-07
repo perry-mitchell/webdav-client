a141f0017fa4d74914ff34263524b24dfd56753c
Perry Mitchell <perry@perrymitchell.net>
2025-06-07 12:59:14 +0300

    Fix all node tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,25 +4,7 @@ export default defineConfig({
     test: {
         fileParallelism: false,
         include: [
-            "test/node/operations/copyFile.spec.ts",
-            "test/node/operations/createDirectory.spec.ts",
-            "test/node/operations/createReadStream.spec.ts",
-            "test/node/operations/createWriteStream.spec.ts",
-            "test/node/operations/customRequest.spec.ts",
-            "test/node/operations/deleteFile.spec.ts",
-            "test/node/operations/exists.spec.ts",
-            "test/node/operations/getDirectoryContents.spec.ts",
-            "test/node/operations/getFileContents.spec.ts",
-            "test/node/operations/getFileDownloadLink.spec.ts",
-            "test/node/operations/getFileUploadLink.spec.ts",
-            "test/node/operations/getQuota.spec.ts",
-            "test/node/operations/lock.spec.ts",
-            "test/node/operations/moveFile.spec.ts",
-            "test/node/operations/partialUpdateFileContents.spec.ts",
-            "test/node/operations/putFileContents.spec.ts",
-            "test/node/operations/search.spec.ts",
-            "test/node/operations/stat.spec.ts",
-            // "test/node/**/*.spec.ts"
+            "test/node/**/*.spec.ts"
         ]
     },
 });
